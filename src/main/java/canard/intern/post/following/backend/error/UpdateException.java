4d6f6db302df9a525a781818f9c48ee9c2fec595
matthcol <matthias.colin@gmail.com>
2022-12-16 16:13:09 +0100

    adapt UpdateException in 418 http status

--- src/main/java/canard/intern/post/following/backend/error/UpdateException.java
+++ src/main/java/canard/intern/post/following/backend/error/UpdateException.java
@@ -1,5 +1,9 @@
 package canard.intern.post.following.backend.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
 public class UpdateException extends RuntimeException {
     public UpdateException() {
     }
